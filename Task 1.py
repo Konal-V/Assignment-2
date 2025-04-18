ca26c5de4493d2821adf1cee4142975aae7ba079
Konal-V <konalvasisth0723@gmail.com>
2025-04-19 00:40:10 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,22 @@
+'''
+a=int(input("Enter a number:"))
+if a%2==0:
+    print(f"{a} is an even number.")
+else:
+    print(f"{a} is an odd number.")
+'''
+'''
+a=int(input("Enter the number:"))
+if a/2==a//2:
+    print(f"{a} is an even number")
+else:
+    print(f"{a} is an odd number")
+'''
+'''
+a=int(input("Enter the number:"))
+b=a/2
+if b==int(b):
+    print(f"{a} is an even number.")
+else:
+    print(f"{a} is an odd number")
+'''
---
+++ task 2.py
@@ -0,0 +1,6 @@
+'''
+for i in range(1,51):
+    print(i)
+i=sum(range(1,51))
+print(f"The sum of numbers from 1 to 50 is:{i}")
+'''
